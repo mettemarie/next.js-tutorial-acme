2d44636166bd2d9bb2f5f432e2cb1ebb0cbd69a9
Mette-Marie <mms@impact.dk>
2024-09-17 10:53:42 +0200

    Refactor data fetching functions to remove artificial delay

--- nextjs-dashboard/app/lib/data.ts
+++ nextjs-dashboard/app/lib/data.ts
@@ -11,15 +11,9 @@ import { formatCurrency } from './utils';
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
-    console.log('Fetching revenue data...');
-    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
-
-    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -30,7 +24,7 @@ export async function fetchRevenue() {
 
 export async function fetchLatestInvoices() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+
 
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -39,7 +33,7 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-      console.log('Data fetch completed after 3 seconds.');
+
 
     const latestInvoices = data.rows.map((invoice) => ({
       ...invoice,
@@ -54,7 +48,7 @@ export async function fetchLatestInvoices() {
 
 export async function fetchCardData() {
   try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+
 
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
